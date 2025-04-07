c33ed681a65743e8cc073078e3ae4a34144a34a4
Matheus-Domingues98 <matheusmdomingues55@gmail.com>
2025-04-06 22:56:53 -0300

    Implement account creation functionality with user input prompts

--- conta_banco/src/contaTerminal.java
+++ conta_banco/src/contaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o numero da conta: ");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o numero da agencia: ");
+        String numeroAgencia = scanner.nextLine();
+
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o saldo da conta: ");
+        double saldoConta = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.");
     }
 }
